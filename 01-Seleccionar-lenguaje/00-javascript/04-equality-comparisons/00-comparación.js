8bb69ade5ee2cc6e100b2fcc00c134b9c6c48d56
Luxorion <benjaminbeccari.1@gmail.com>
2025-03-31 18:28:19 -0300

    Operadores de comparacion

---
+++ 01-Seleccionar-lenguaje/00-javascript/04-equality-comparisons/00-comparación.js
@@ -0,0 +1,21 @@
+/*
+Comparación y Operadores Lógicos
+
+https://www.w3schools.com/js/js_comparisons.asp
+
+*/
+
+
+// Operadores de comparacion
+
+// Igual que con operador == 
+
+let x = 5
+x == "5" // True
+
+// Estrictamente igual que con operador === 
+
+x === "5" // False
+
+
+
